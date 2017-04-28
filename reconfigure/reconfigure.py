cbddd9ae09320dfcec6727c60fc9910860169131
M. Blahetek <MBlahetek@users.noreply.github.com>
2017-04-28 11:55:32 +0200

    test

--- reconfigure/reconfigure.py
+++ reconfigure/reconfigure.py
@@ -5,7 +5,6 @@ Created on 28.04.2017
 '''
 
 import docker
-
-client = docker.from_environment()
-
-client.container.list()
+client = docker.from_env()
+for container in client.containers.list():
+  print container.id
