79b879597949caaf1da97a3a437a495297dfc4a9
M. Blahetek <MBlahetek@users.noreply.github.com>
2017-05-01 13:56:01 +0200

    get ovs bridge

--- reconfigure/reconffunctions.py
+++ reconfigure/reconffunctions.py
@@ -4,3 +4,13 @@ Created on 28.04.2017
 @author: Matthias Blahetek
 '''
 
+def get_ovs_bridge_name():
+    try:
+        for line in open('/proc/net/dev', 'r'):
+            if "ovsbr" in line:
+                bridge_name = line.split(":", 1)[0]
+                return bridge_name
+    finally:
+        bridge_name = None
+        return bridge_name
+        
--- reconfigure/reconfigure.py
+++ reconfigure/reconfigure.py
@@ -7,17 +7,22 @@ Created on 28.04.2017
 import docker
 import reconffunctions as rf
 
-
-
 client = docker.from_env()
 
 bridge_docker = client.networks.list('bridge')[0]
 bridge_docker_container = bridge_docker.containers
+container_list = []
 
 for i in bridge_docker_container:
     attributes = i.attrs
-    name = attributes["Name"]
+    name = str(attributes["Name"])
     name = name[1:]
-    ip = attributes["NetworkSettings"]["Networks"]["bridge"]["IPAddress"]
+    ip = str(attributes["NetworkSettings"]["Networks"]["bridge"]["IPAddress"])
+    container_list.append((name, ip))
     
-    break
+ovs_bridge_name = rf.get_ovs_bridge_name
+if ovs_bridge_name is None:
+    raise "no ovs bridge found"
+print ovs_bridge_name
+
+    
