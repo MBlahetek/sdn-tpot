ab27f69cd3aa3ad80b15ba82074f42624f7fe027
M. Blahetek <MBlahetek@users.noreply.github.com>
2017-05-02 09:37:56 +0200

    access iptables

--- reconfigure/reconfigure.py
+++ reconfigure/reconfigure.py
@@ -3,7 +3,7 @@ Created on 28.04.2017
 
 @author: Matthias Blahetek
 '''
-
+import iptc
 import docker
 import reconffunctions as rf
 
@@ -15,7 +15,7 @@ bridge_docker_container = bridge_docker.containers
 container_ip_list = []
 
 #Connect all docker0 containers to the ovs bridge
-
+"""
 for i in bridge_docker_container:
     attributes = i.attrs
     name = str(attributes["Name"])
@@ -29,14 +29,17 @@ for i in container_ip_list:
     container = client.containers.get(name)
     ip_bridge_ovs = str(container.attrs["NetworkSettings"]["Networks"]["sdnnet"]["IPAddress"])
     i = i.append(ip_bridge_ovs)
-    
-print container_ip_list    
-    
+"""        
 ovs_bridge_name = rf.get_ovs_bridge_name()
 if ovs_bridge_name is None:
     raise NameError("no ovs bridge found")
 print ovs_bridge_name
 
+table_filter = iptc.Table(iptc.Table.FILTER)
+table_nat = iptc.Table(iptc.Table.NAT)
 
+chains_filter = table_filter.chains
+chains_nat = table_nat.chains
 
-    
+print chains_filter
+print chains_nat
