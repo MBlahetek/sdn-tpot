03d2e2e5fda99fdeaff2f9ad4f926973093db0a9
M. Blahetek <MBlahetek@users.noreply.github.com>
2017-05-02 15:59:26 +0200

    complete test

--- reconfigure/reconffunctions.py
+++ reconfigure/reconffunctions.py
@@ -4,8 +4,68 @@ Created on 28.04.2017
 @author: Matthias Blahetek
 '''
 
+import iptc
+
 def get_ovs_bridge_name():
     for line in open('/proc/net/dev', 'r'):
         if "ovsbr" in line:
             bridge_name = line.split(":", 1)[0]
-            return str(bridge_name) 
+            return str(bridge_name)
+
+def update_iptables_chain(chain, ip_map):
+    rules = chain.rules
+    new_rules = []
+    
+    for r in rules:
+        new_rule = iptc.Rule()
+        
+        if r.matches:
+            match = r.matches[0]
+            new_rule.add_match(match)
+            
+        if r.in_interface is not None:
+            if r.in_interface == "docker0":
+                new_rule.in_interface = ovs_bridge_name
+            elif r.in_interface == "!docker0":
+                new_rule.in_interface = "!" + ovs_bridge_name
+        if r.out_interface is not None:
+            if r.out_interface == "docker0":
+                new_rule.out_interface = ovs_bridge_name
+            elif r.out_interface == "!docker0":
+                new_rule.out_interface = "!" + ovs_bridge_name
+        
+        if r.src == "172.17.0.0/255.255.0.0":
+            new_rule.src = "172.18.0.0/255.255.0.0" 
+        elif r.src != "0.0.0.0/0.0.0.0":
+            old_ip = r.src.split("/", 1)[0]
+            old_subnetmask = r.src.split("/", 1)[1]
+            for ip in ip_map:
+                if ip[1] == old_ip:
+                    new_rule.src = ip[2] + "/" + old_subnetmask
+                    break
+        else:
+            new_rule.src = "0.0.0.0/0.0.0.0"
+                
+        if r.dst == "172.17.0.0/255.255.0.0":
+            new_rule.dst = "172.18.0.0/255.255.0.0" 
+        elif r.dst != "0.0.0.0/0.0.0.0":
+            old_ip = r.dst.split("/", 1)[0]
+            old_subnetmask = r.dst.split("/", 1)[1]
+            for ip in ip_map:
+                if ip[1] == old_ip:
+                    new_rule.dst = ip[2] + "/" + old_subnetmask
+                    break
+        else:
+            new_rule.dst = "0.0.0.0/0.0.0.0"
+        
+        new_rule.protocol = r.protocol
+        
+        new_rule.target = r.target
+           
+        new_rules.append(new_rule)
+    
+    for r in rules:
+        chain.delete_rule(r)
+
+    for r in new_rules:
+        chain.append_rule(r)
--- reconfigure/reconfigure.py
+++ reconfigure/reconfigure.py
@@ -15,7 +15,7 @@ bridge_docker_container = bridge_docker.containers
 container_ip_list = []
 
 #Connect all docker0 containers to the ovs bridge
-"""
+
 for i in bridge_docker_container:
     attributes = i.attrs
     name = str(attributes["Name"])
@@ -29,40 +29,23 @@ for i in container_ip_list:
     container = client.containers.get(name)
     ip_bridge_ovs = str(container.attrs["NetworkSettings"]["Networks"]["sdnnet"]["IPAddress"])
     i = i.append(ip_bridge_ovs)
-"""        
+        
 ovs_bridge_name = rf.get_ovs_bridge_name()
 if ovs_bridge_name is None:
     raise NameError("no ovs bridge found")
 
 table_filter = iptc.Table(iptc.Table.FILTER)
-#table_nat = iptc.Table(iptc.Table.NAT)
-
+table_nat = iptc.Table(iptc.Table.NAT)
 chain_filter_forward = iptc.Chain(table_filter, "FORWARD")
 chain_filter_docker = iptc.Chain(table_filter, "DOCKER")
-#chains_nat = table_nat.chains
+chain_nat_postrouting = iptc.Chain(table_nat, "POSTROUTING")
+chain_nat_docker = iptc.Chain(table_nat, "DOCKER")
 
-rules_filter_forward = chain_filter_forward.rules
-new_rules = []
-for r in rules_filter_forward:
-    new_rule = iptc.Rule()
-    
-    if r.matches:
-        match = r.matches[0]
-        new_rule.add_match(match)
-    if r.in_interface is not None:
-        if r.in_interface == "docker0":
-            new_rule.in_interface = ovs_bridge_name
-        elif r.in_interface == "!docker0":
-            new_rule.in_interface = "!" + ovs_bridge_name
-    if r.out_interface is not None:
-        if r.out_interface == "docker0":
-            new_rule.out_interface = ovs_bridge_name
-        elif r.out_interface == "!docker0":
-            new_rule.out_interface = "!" + ovs_bridge_name
-    new_rule.target = r.target
-    new_rules.append(new_rule)
-    
-for i in new_rules:
-    chain_filter_forward.append_rule(i)
-#print chains_filter
-#print chains_nat
+rf.update_iptables_chain(chain_filter_forward, container_ip_list)
+rf.update_iptables_chain(chain_filter_docker, container_ip_list)
+rf.update_iptables_chain(chain_nat_postrouting, container_ip_list)
+rf.update_iptables_chain(chain_nat_docker, container_ip_list)
+
+for i in container_ip_list:
+    name = i[0]
+    bridge_docker.disconnect(name)
