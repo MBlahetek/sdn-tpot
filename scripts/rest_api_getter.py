cf1781ae0512f5f880ea318af6d4aeb2812244d2
M. Blahetek <MBlahetek@users.noreply.github.com>
2017-05-08 10:46:13 +0200

    added get_ip_port_mapping

--- scripts/rest_api_getter.py
+++ scripts/rest_api_getter.py
@@ -18,7 +18,19 @@ class RestApiGetter(object):
             if entity["ipv4"] == ["172.18.0.1"]:
                 switch = entity["attachmentPoint"][0]["switch"]
                 break
-        return switch  
+        return switch
+    
+    def get_ip_port_mapping(self):
+        path = "/wm/device/"
+        ret = self.rest_call(path, 'GET')
+        json_object = json.loads(ret[2])
+        map = []
+        for entity in json_object["devices"]:
+            if entity["ipv4"] != []:
+                ip = entity["ipv4"][0]
+                port = entity["attachmentPoint"][0]["port"]
+                map.append((ip,port))
+        return map
 
     def rest_call(self, path, action):
         conn = httplib.HTTPConnection(self.server, 9090)
