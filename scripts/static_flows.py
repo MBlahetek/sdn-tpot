2e92ebc5f7cb8813344c1ecf6d6e60eba7e4003d
M. Blahetek <MBlahetek@users.noreply.github.com>
2017-05-08 11:40:32 +0200

    added: ip switchport mapping, inital ping traffic, floodlight rules

--- scripts/static_flows.py
+++ scripts/static_flows.py
@@ -1,10 +1,6 @@
 import static_entry_pusher as pusher
+import rest_api_getter
 import docker
-
-"""
-TODO get ip/switch-port mapping via: curl http://172.18.0.5:9090/wm/device/
-TODO docker exec ping for making them visible
-"""
 
 client = docker.from_env()
 floodlight = client.containers.get("floodlight")
@@ -14,11 +10,68 @@ floodlight_ip = floodlight.attrs["NetworkSettings"]["Networks"]["sdnnet"]["IPAdd
 glastopf_ip = glastopf.attrs["NetworkSettings"]["Networks"]["sdnnet"]["IPAddress"]
 cowrie_ip = cowrie.attrs["NetworkSettings"]["Networks"]["sdnnet"]["IPAddress"]
 
-push = pusher.StaticEntryPusher(floodlight_ip)
+
+# generate traffic to make the hosts visible for the Controller
+# important for getting the ip - switch port - mapping in the next step
+
+
+bridge_ovs = client.networks.list('sdnnet')[0]
+k = 2
+for i in bridge_ovs.containers:
+    cmd_str = "ping -c 4 172.18.0." + str(k)
+    cowrie.exec_run(cmd_str)
+    k += 1
+    
+# get ip - switch port - mapping for dynamic pro-activ flow generation
+
+rest_api = rest_api_getter.RestApiGetter(floodlight_ip)
+ip_port_map = rest_api.get_ip_port_mapping()
+switch = rest_api.get_switch()
+
+for i in ip_port_map:
+    if i[0] == glastopf_ip:
+        glastopf_port = i[1]
+    elif i[0] == cowrie_ip:
+        cowrie_port = i[1]
+    elif i[0] == floodlight_ip:
+        floodlight_port = i[1]
+
+# write all the pro-active flow rules
+
 flows = []
 
+floodlight_rest = {
+    'switch': switch,
+    "name":"floodlight_rest",
+    "cookie":"0",
+    "priority":"30000",
+    "in_port":"local",
+    "eth_type":"0x0800",
+    "ip_proto":"0x06",
+    "ipv4_src":"0.0.0.0/0", # TODO restrict on admin ip range
+    "ipv4_dst": floodlight_ip + "/32",
+    "tcp_dst":"9090",
+    "active":"true",
+    "actions":"output=" + floodlight_port
+    }
+flows.append(floodlight_rest)
+
+floodlight_drop = {
+    'switch': switch,
+    "name":"floodlight_drop",
+    "cookie":"0",
+    "priority":"1000",
+    "in_port":"local",
+    "eth_type":"0x0800",
+    "ipv4_src":"0.0.0.0/0",
+    "ipv4_dst": floodlight_ip + "/32",
+    "active":"true",
+    "actions":""
+    }
+flows.append(floodlight_drop)
+
 glastopf_http = {
-    'switch':"00:00:42:41:c8:da:11:40",
+    'switch': switch,
     "name":"glastopf_http",
     "cookie":"0",
     "priority":"30000",
@@ -29,12 +82,12 @@ glastopf_http = {
     "ipv4_dst": glastopf_ip + "/32",
     "tcp_dst":"80",
     "active":"true",
-    "actions":"output=3"
+    "actions":"output=" + glastopf_port
     }
 flows.append(glastopf_http)
 
 glastopf_drop = {
-    'switch':"00:00:42:41:c8:da:11:40",
+    'switch': switch,
     "name":"glastopf_drop",
     "cookie":"0",
     "priority":"1000",
@@ -48,7 +101,7 @@ glastopf_drop = {
 flows.append(glastopf_drop)
  
 cowrie_ssh = {
-    'switch':"00:00:42:41:c8:da:11:40",
+    'switch': switch,
     "name":"cowrie_ssh",
     "cookie":"0",
     "priority":"30000",
@@ -59,12 +112,12 @@ cowrie_ssh = {
     "ipv4_dst": cowrie_ip + "/32",
     "tcp_dst":"2222",
     "active":"true",
-    "actions":"output=3"
+    "actions":"output=" + cowrie_port
     }
 flows.append(cowrie_ssh)
  
 cowrie_telnet = {
-    'switch':"00:00:42:41:c8:da:11:40",
+    'switch': switch,
     "name":"cowrie_telnet",
     "cookie":"0",
     "priority":"30000",
@@ -75,12 +128,12 @@ cowrie_telnet = {
     "ipv4_dst": cowrie_ip + "/32",
     "tcp_dst":"2223",
     "active":"true",
-    "actions":"output=3"
+    "actions":"output=" + cowrie_port
     }
 flows.append(cowrie_telnet)
 
 cowrie_drop = {
-    'switch':"00:00:42:41:c8:da:11:40",
+    'switch': switch,
     "name":"cowrie_drop",
     "cookie":"0",
     "priority":"1000",
@@ -92,6 +145,10 @@ cowrie_drop = {
     "actions":""
     }
 flows.append(cowrie_drop)
+
+# send the pro-activ flow rules to the controller
+
+push = pusher.StaticEntryPusher(floodlight_ip)
 
 for flow in flows:
     push.set(flow)
