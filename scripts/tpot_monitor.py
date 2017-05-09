3922284c89111b3566595f5ddaaba1c3ba7396fc
M. Blahetek <MBlahetek@users.noreply.github.com>
2017-05-09 11:34:02 +0200

    new: add_ids

--- scripts/tpot_monitor.py
+++ scripts/tpot_monitor.py
@@ -9,6 +9,12 @@ import json
 import docker
 import time
 
+PORTS = {
+    "2222":"ssh",
+    "2223":"telnet",
+    "80":"http"   
+    }
+
 class TpotMonitor(object):
     
     def __init__(self, controller, switch, polling):
@@ -24,8 +30,47 @@ class TpotMonitor(object):
         return json_flows
     
     def add_ids(self, flow):
-        raise NotImplementedError("TODO")
-  
+        new_priority = int(flow["priority"]) + 100
+        in_port = flow["match"]["in_port"]
+        eth_type = flow["match"]["eth_type"]
+        ip_proto = flow["match"]["ip_proto"]
+        ipv4_dst = flow["match"]["ipv4_dst"]
+        tcp_dst = flow["match"]["tcp_dst"]
+        actions = flow["instructions"]["instruction_apply_actions"]["actions"]
+        
+        name = "suricata_" + PORTS[tcp_dst]
+        client = docker.from_env()
+        container = client.containers.run('dtagdevsec/suricata', detach=True, name=name, networks=["sdnnet"])
+        container.exec_run("ping -c 3 172.18.0.1")
+        container_ip = container.attrs["NetworkSettings"]["Networks"]["sdnnet"]["IPAddress"]
+        ip_port_map = self.rest_api.get_ip_port_mapping()
+        for i in ip_port_map:
+            if i[0] == container_ip:
+                container_switch_port = i[1]
+                break
+        actions = actions + ",output=" + container_switch_port
+        
+        ids_flow = {
+            'switch': self.switch,
+            "name": name,
+            "cookie":"0",
+            "priority": str(new_priority),
+            "in_port": in_port,
+            "eth_type": eth_type,
+            "ip_proto":ip_proto,
+            "ipv4_src":"0.0.0.0/0",
+            "ipv4_dst": ipv4_dst,
+            "tcp_dst": tcp_dst,
+            "active":"true",
+            "actions": actions
+            }
+        
+        ids_flow_drop = "TODO"
+        
+        push = static_entry_pusher.StaticEntryPusher(self.controller)
+        push.set(new_temp_flow)
+        print "flow entry set: " + name
+        
     def remove_ids(self, flow):
         raise NotImplementedError("TODO")
         
@@ -47,13 +92,26 @@ class TpotMonitor(object):
                 if int(flow["priority"]) > 1000 and int(flow["priority"]) <= 32700:
                     new_flow = False
                     package_count = int(flow["packet_count"])
+                    
                     for i in old_stats:                    
                         if flow["match"] == i[0] and flow["priority"] == i[1]:
                             old_count = i[2]
                             old_package_increase = i[3]
                             new_package_increase = package_count - old_count
-                            if new_package_increase != 0 and int(flow["priority"]) == 15000: # inital traffic causes start of a specific ids
+                            # inital traffic causes start of a specific ids
+                            if new_package_increase > 0 and int(flow["priority"]) == 15000:
                                 self.add_ids(flow)
+                            # if there's no traffic over the last k polling intervalls, the ids + flow will be removed
+                            if new_package_increase == 0 and int(flow["priority"]) == 15100:
+                                if len(i) == 4:
+                                    k = 1
+                                    i.append(k)
+                                else:
+                                    i[4] += 1
+                                    if i[4] == 3:
+                                        self.remove_ids(flow)
+                                        del i[4]
+                                        
                             i[2] = package_count
                             i[3] = new_package_increase
                             new_flow = False
